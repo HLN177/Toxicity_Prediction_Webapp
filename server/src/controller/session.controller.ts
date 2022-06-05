d739404996e343e6b304a15ce023a90f9d98a86b
HLN177 <harryleen177@gmail.com>
2022-06-05 03:27:45 +0100

    feat: get | | delete ->  user session

--- server/src/controller/session.controller.ts
+++ server/src/controller/session.controller.ts
@@ -1,43 +1,86 @@
 import { Request, Response } from "express";
 import { validatePassword } from "../service/user.service";
-import { createSession } from "../service/session.service";
+import { createSession, findSessions, updateSession } from "../service/session.service";
 import { signJwt } from "../utils/jwt.utils";
 import config from 'config';
 
-export async function createUserSessionHandler(
+/**
+ * create user session
+ * 1. validate user's password
+ * 2. create a sesion
+ * 3. create an access token
+ * 4. create a refresh token
+ * 5. return access & refresh tokens
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+async function createUserSessionHandler(
   req: Request,
   res: Response
 ) {
-  // Validate user's password
+  // 1. validate user's password
   const user =  await validatePassword(req.body);
   if (!user) {
     return res.status(401).send("Invalid email or password");
   }
-  // create a sesion
+  // 2. create a sesion
   const session = await createSession(user._id, req.get("user-agent") || "")
 
-  // create an access token
+  // 3. create an access token
   const accessToken = signJwt(
-    {
-      ...user,
-      session: session._id
-    },
-    {
-      expiresIn: config.get('accessTokenTtl') // 15 minutes
-    }
+    { ...user, session: session._id },
+    { expiresIn: config.get('accessTokenTtl') } // 15 minutes
   );
 
-  // create a refresh token
+  // 4. create a refresh token
   const refreshToken = signJwt(
-    {
-      ...user,
-      session: session._id
-    },
-    {
-      expiresIn: config.get('refreshTokenTtl') // 15 minutes
-    }
+    { ...user, session: session._id },
+    { expiresIn: config.get('refreshTokenTtl') } // 1 year
   );
-  // return access & refresh tokens
+  // 5. return access & refresh tokens
 
   return res.send({accessToken, refreshToken});
 }
+
+/**
+ * find session info by user id in db
+ * How to know the user id is? 
+ * Ideallythe user will be on request object
+ * middleware needed (deserializeUser)
+ * @param req 
+ * @param res 
+ */
+async function getUserSessionsHandler(
+  req: Request,
+  res: Response
+) {
+  const userId = res.locals.user._id;
+  const sessions = await findSessions({user: userId, valid: true});
+  return res.send(sessions);
+}
+
+async function deleteUserSessionHandler(
+  req: Request,
+  res: Response
+) {
+  const sessionId = res.locals.user.session;
+  const query = {
+    _id: sessionId
+  };
+  const update = {
+    valid: false // reIssueAccessToken function will check this property to determind if reIssue or not
+  };
+  await updateSession(query, update);
+
+  return res.send({
+    accessToken: null,
+    refreshToken: null
+  });
+}
+
+export {
+  createUserSessionHandler,
+  getUserSessionsHandler,
+  deleteUserSessionHandler
+};
